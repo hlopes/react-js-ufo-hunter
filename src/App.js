d85f805a3edd40a22aa49dd0c839988d0c13ebbc
Hugo Lopes <hugo.rodrigue.lopes@gmail.com>
2024-01-01 09:53:12 +0000

    change the sounds volume

--- src/App.js
+++ src/App.js
@@ -1,77 +1,75 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 
 // Game
-import GameBasics from './game/GameBasics';
-import Sounds from './game/Sounds';
+import GameBasics from './game/GameBasics'
 
 // Utils
-import throttle from 'lodash/throttle';
-import keyCodes from './utils/keys';
+import throttle from 'lodash/throttle'
+import keyCodes from './utils/keys'
 
 // Styles
-import './App.css';
+import './App.css'
 
 const resize = (canvas) => {
-    if (!canvas) {
-        return;
-    }
+  if (!canvas) {
+    return
+  }
 
-    const height = window.innerHeight - 20;
+  const height = window.innerHeight - 20
 
-    const ratio = canvas.width / canvas.height;
-    const width = height * ratio;
+  const ratio = canvas.width / canvas.height
+  const width = height * ratio
 
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
-};
+  canvas.style.width = width + 'px'
+  canvas.style.height = height + 'px'
+}
 
 function App() {
-    useEffect(() => {
-        if (document) {
-            const canvas = document.getElementById('ufoCanvas');
-            canvas.width = 900;
-            canvas.height = 750;
+  useEffect(() => {
+    if (document) {
+      const canvas = document.getElementById('ufoCanvas')
+      canvas.width = 900
+      canvas.height = 750
 
-            const play = new GameBasics(canvas);
-            play.start();
+      const play = new GameBasics(canvas)
+      play.start()
 
-            const handleResize = () => resize(canvas);
-            const throttledHandleResize = throttle(() => resize(canvas), 100);
-            const handleKeyDown = (e) => {
-                const keyboardCode = e.which || e.keyCode;
+      const handleResize = () => resize(canvas)
+      const throttledHandleResize = throttle(() => resize(canvas), 100)
+      const handleKeyDown = (e) => {
+        const keyboardCode = e.which || e.keyCode
 
-                if (Object.values(keyCodes).includes(keyboardCode)) {
-                    e.preventDefault();
-                }
-
-                play.keyDown(keyboardCode);
-            } ;
-            const handleKeyUp = (e) => {
-                const keyboardCode = e.which || e.keyCode;
-
-                play.keyUp(keyboardCode);
-            } ;
-
-            window.addEventListener('load', handleResize, false);
-            window.addEventListener('resize', throttledHandleResize, true);
-            window.addEventListener('keydown', handleKeyDown);
-            window.addEventListener('keyup', handleKeyUp);
-
-            return () => {
-                window.removeEventListener('load', handleResize);
-                window.removeEventListener('resize', throttledHandleResize);
-                window.removeEventListener('keydown', handleKeyDown);
-                window.removeEventListener('keyup', handleKeyUp);
-            }
+        if (Object.values(keyCodes).includes(keyboardCode)) {
+          e.preventDefault()
         }
 
-    });
+        play.keyDown(keyboardCode)
+      }
+      const handleKeyUp = (e) => {
+        const keyboardCode = e.which || e.keyCode
+
+        play.keyUp(keyboardCode)
+      }
+
+      window.addEventListener('load', handleResize, false)
+      window.addEventListener('resize', throttledHandleResize, true)
+      window.addEventListener('keydown', handleKeyDown)
+      window.addEventListener('keyup', handleKeyUp)
+
+      return () => {
+        window.removeEventListener('load', handleResize)
+        window.removeEventListener('resize', throttledHandleResize)
+        window.removeEventListener('keydown', handleKeyDown)
+        window.removeEventListener('keyup', handleKeyUp)
+      }
+    }
+  })
 
   return (
     <div className="App">
-      <canvas id={ 'ufoCanvas'}></canvas>
+      <canvas id={'ufoCanvas'}></canvas>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/game/Sounds.js
+++ src/game/Sounds.js
@@ -1,51 +1,54 @@
-import { Howl } from 'howler';
+import { Howl } from 'howler'
 
 // Sounds
-import shot from '../sounds/shot.mp3';
-import explosion from '../sounds/explosion.mp3';
-import ufoDeath from '../sounds/ufo-dead.mp3';
+import shot from '../sounds/shot.mp3'
+import explosion from '../sounds/explosion.mp3'
+import ufoDeath from '../sounds/ufo-dead.mp3'
 
 class Sounds {
-    constructor() {
-        this.muted = false;
+  constructor() {
+    this.muted = false
+  }
+
+  init() {
+    this.shotSound = new Howl({
+      src: shot,
+      volume: 0.2,
+    })
+    this.explosionSound = new Howl({
+      src: explosion,
+      volume: 0.2,
+    })
+    this.ufoDeathSound = new Howl({
+      src: ufoDeath,
+      volume: 0.2,
+    })
+  }
+
+  playSound(soundName) {
+    if (this.muted) {
+      return
     }
 
-    init() {
-        this.shotSound = new Howl({
-            src: shot
-        });
-        this.explosionSound = new Howl({
-            src: explosion
-        });
-        this.ufoDeathSound = new Howl({
-            src: ufoDeath
-        });
+    switch (soundName) {
+      case 'shot':
+        this.shotSound.play()
+        this.shotSound.currentTime = 0
+        break
+      case 'ufoDeath':
+        this.ufoDeathSound.play()
+        this.ufoDeathSound.currentTime = 0
+        break
+      default:
+        this.explosionSound.play()
+        this.explosionSound.currentTime = 0
+        return
     }
+  }
 
-    playSound(soundName) {
-        if (this.muted) {
-            return;
-        }
-
-        switch (soundName) {
-            case 'shot':
-                this.shotSound.play();
-                this.shotSound.currentTime = 0;
-                break;
-            case 'ufoDeath':
-                this.ufoDeathSound.play();
-                this.ufoDeathSound.currentTime = 0;
-                break;
-            default:
-                this.explosionSound.play();
-                this.explosionSound.currentTime = 0;
-                return;
-        }
-    }
-
-    mute() {
-        this.muted = !this.muted;
-    }
+  mute() {
+    this.muted = !this.muted
+  }
 }
 
-export default Sounds;
+export default Sounds
